018533c138b7f589040e6fdbd93836aa6fea497f
Terrence Matthews <51449778+Maker2000@users.noreply.github.com>
2025-04-11 18:26:26 -0500

    fixed issue with being unable to login due to password comparison (#6)

--- app/api/admin/password-reset/route.ts
+++ app/api/admin/password-reset/route.ts
@@ -4,7 +4,7 @@ import { ResetPasswordDto, validatePasswordReset } from "@/app/models/user";
 import { Contract, Validation } from "@/lib/contracts";
 import connectDB from "@/lib/mongose-client";
 import { loadAdminCookie } from "@/lib/server-functions";
-import { compare, genSalt, hash } from "bcrypt";
+import { compareSync, genSaltSync, hashSync } from "bcrypt";
 import { User } from "@/app/models/user.mongoose";
 
 export async function PUT(req: NextRequest) {
@@ -17,12 +17,12 @@ export async function PUT(req: NextRequest) {
     await connectDB();
     let user = await User.findById(contextUser.id);
     Validation.requireNotNull(user, "Incorrect username-password combination");
-    let match = await compare(payload.oldPassword!, user!.password);
-    let newPasswordMatch = await compare(payload.newPassword!, user!.password);
+    let match = compareSync(payload.oldPassword!, user!.password);
+    let newPasswordMatch = compareSync(payload.newPassword!, user!.password);
     Validation.requires(match, "Incorrect username-password combination");
     Validation.requires(!newPasswordMatch, "New password cannot be the same as old password");
-    let salt = await genSalt(20);
-    let newPassword = await hash(payload.newPassword!, salt);
+    let salt = genSaltSync(20);
+    let newPassword = hashSync(payload.newPassword!, salt);
     let newUser = await User.updateOne(
       { _id: user!._id },
       {
