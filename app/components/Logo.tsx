11a7239e3f8aff311c45f0e18d5e39b062e3f100
Terrence Matthews <matthewsterrence1996@gmail.com>
2025-06-23 20:23:51 -0500

    added carousel to home page

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Image from "next/image";
 function Logo({ size, leaf = true }: { size: number; leaf?: boolean }) {
-  return (
-    <div>
-      <Image src={leaf ? "/ourLogo.svg" : "/ourLogo-no-leaf.svg"} alt="logo" width={size} height={size} className="bg-white  p-1 rounded-full" />
-    </div>
-  );
+  return <Image src={leaf ? "/ourLogo.svg" : "/ourLogo-no-leaf.svg"} alt="logo" width={size} height={size} className="bg-white  p-1 rounded-full" />;
 }
 
 export default Logo;
