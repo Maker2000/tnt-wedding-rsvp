9939f351a09c0bd0d695d5ba434307833aab9c88
Terrence Matthews <matthewsterrence1996@gmail.com>
2025-06-23 20:23:51 -0500

    added carousel to home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import Image from "next/image";
 import Logo from "../components/Logo";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-full gap-5 text-center">
-      {/* <div>Save the date</div> */}
+    <div className="flex flex-col justify-center items-center gap-5 text-center">
       <Logo size={300} />
       <h1 className="flex flex-col text-4xl font-bold font-shadows-into-light gap-2">
         <div>We're</div>
         <div>Getting Married</div>
-        <div></div>
       </h1>
       <div className="font-shadows-into-light text-5xl">T & R</div>
-      {/* <div>(add date)</div> */}
+      <Carousel folder="carousel/photo-shoot" prefix="shoot" count={17} />
     </div>
   );
 }
